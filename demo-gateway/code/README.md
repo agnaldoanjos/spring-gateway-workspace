8e2867c8d6c6d5ed0ab8bb00ac78129541be1008
agnaldoanjos <agnaldo.anjos@gmail.com>
2022-06-30 00:08:17 -0300

    Codigo inicial

---
+++ demo-gateway/code/README.md
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=zPVBw305vlI
+
+https://www.javainuse.com/spring/cloud-gateway
+
+https://docs.spring.io/spring-cloud-gateway/docs/current/reference/html/
---
+++ demo-gateway/code/src/main/java/br/home/zuul/demozuul/DemoZuulApplication.java
@@ -0,0 +1,13 @@
+package br.home.zuul.demozuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZuulApplication.class, args);
+	}
+
+}
