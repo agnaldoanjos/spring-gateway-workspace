56e2647f741c61f0d3fd7fe786e86a343f241127
agnaldoanjos <agnaldo.anjos@gmail.com>
2022-06-30 00:26:03 -0300

    Ajuste da nomenclatura do APP

--- demo-gateway/code/src/main/java/br/home/zuul/demozuul/DemoApplication.java
+++ demo-gateway/code/src/main/java/br/home/zuul/demozuul/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoZuulApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoZuulApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
